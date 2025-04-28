3e0ac82fcfe36c22b219da666c7d91f4327d043c
Diana <kostiuchenko.diana@lll.kpi.ua>
2025-04-28 09:38:52 +0300

    fixed sum

--- ПО_лаб_2/ПО_лаб_2.cpp
+++ ПО_лаб_2/ПО_лаб_2.cpp
@@ -9,13 +9,13 @@ const std::vector<int> NUM_THREADS = { 4, 8, 16, 32, 64, 128, 320 };
 int SIZE;
 std::vector<int> data;
 std::mutex mtx;
-std::atomic<int> atomic_sum(0);
+std::atomic<long long> atomic_sum(0);
 std::atomic<int> atomic_min(INT_MAX);
 
 void generate_data() {
     data.resize(SIZE);
     for (int i = 0; i < SIZE; ++i) {
-        data[i] = rand() % 10000;  // Генеруємо випадкові числа від 0 до 9999
+        data[i] = rand() % 1000;  // Генеруємо випадкові числа від 0 до 9999
     }
 }
 
@@ -24,7 +24,7 @@ void sequential(int& sum, int& min_value) {
     sum = 0;
     min_value = INT_MAX;
     for (int num : data) {
-        if (num % 10 == 0) {
+        if (num % 10 == 0) { // чи кратне 10 як треба за умовою
             sum += num;
             if (num < min_value) {
                 min_value = num;
@@ -34,8 +34,8 @@ void sequential(int& sum, int& min_value) {
 }
 
 // Варіант з м’ютексом
-void mutex_version(int& sum, int& min_value, int start, int end) {
-    int local_sum = 0;
+void mutex_version(int& sum, int& min_value, int start, int end) { // виконує кожен потік
+    long long local_sum = 0;
     int local_min = INT_MAX;
     for (int i = start; i < end; ++i) {
         if (data[i] % 10 == 0) {
@@ -45,7 +45,7 @@ void mutex_version(int& sum, int& min_value, int start, int end) {
             }
         }
     }
-    std::lock_guard<std::mutex> lock(mtx);
+    std::lock_guard<std::mutex> lock(mtx); // тільки один потік може модифікувати sum і min_value
     sum += local_sum;
     if (local_min < min_value) {
         min_value = local_min;
@@ -64,9 +64,9 @@ void cas_version(int start, int end) {
             }
         }
     }
-    atomic_sum.fetch_add(local_sum, std::memory_order_relaxed);
-    int old_min = atomic_min.load();
-    while (local_min < old_min && !atomic_min.compare_exchange_weak(old_min, local_min));
+    atomic_sum.fetch_add(local_sum, std::memory_order_relaxed); // кожен потік додає свою суму в глобальну
+    int old_min = atomic_min.load(); //  копія старого мінімуму з пам'яті
+    while (local_min < old_min && !atomic_min.compare_exchange_weak(old_min, local_min)); //if atomic_min = old_min -> local_min else: refresh old_min 
 }
 
 int main() {
@@ -84,8 +84,9 @@ int main() {
     std::cout << "\n" << std::endl;
 
     // Версія з м’ютексом
-    sum = 0, min_value = INT_MAX;
+    
     for (int num_threads : NUM_THREADS) {
+        sum = 0, min_value = INT_MAX;
         start = std::chrono::high_resolution_clock::now();
         std::vector<std::thread> threads;
         int chunk_size = SIZE / num_threads;
@@ -101,9 +102,9 @@ int main() {
 	std::cout << "\n" << std::endl;
 
     // Версія з CAS (cmpxchg)
-    atomic_sum = 0;
-    atomic_min = INT_MAX;
+    
     for (int num_threads : NUM_THREADS) {
+        atomic_sum = 0, atomic_min = INT_MAX;
         start = std::chrono::high_resolution_clock::now();
         std::vector<std::thread> threads;
         int chunk_size = SIZE / num_threads;
@@ -118,3 +119,4 @@ int main() {
 
     return 0;
 }
+
